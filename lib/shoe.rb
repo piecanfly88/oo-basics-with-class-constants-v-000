bec86e145031d240ecdc958e4179c8e7aaa4458a
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-22 08:58:49 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,18 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand, 
+  attr_reader :brand,
   # unique_brand
 
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS.uniq
   end
 
   def brand=(brand)
     @brand = brand
     BRANDS << brand
-    BRANDS.uniq
   end
 
   # def uniq_brand=(uniq_brand)
